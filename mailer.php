53cf11fc2641de849fd22375a8da471abb84e94c
uskeliye <jhumasen.js@gmail.com>
2025-03-08 13:23:07 +0530

    Update mailer.php

--- mailer.php
+++ mailer.php
@@ -17,7 +17,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     // Email headers.
     $headers = "From: your_email@example.com\r\n"; // Update with your sender email
-    $headers .= "Reply-To: paulupamanyu@gmail.com\r\n";
+    $headers .= "Reply-To: your_email@example.com\r\n";
     $headers .= "MIME-Version: 1.0\r\n";
     $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
 
